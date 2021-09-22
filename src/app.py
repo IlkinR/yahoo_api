7cdae9a1ee54f383158b2f2e60dd58797041e055
Ilkin Ramazanov <babalke44@gmail.com>
2021-09-22 19:55:55 +0400

    import funcs in app file instead of module itself

--- src/app.py
+++ src/app.py
@@ -1,21 +1,18 @@
 from fastapi import FastAPI
-import logic
+from logic import extract_all_finances, extract_company_finances
 
 api = FastAPI()
 
 
 @api.get("/finances/")
 def get_all_finances(overall_records: int = 100):
-    print(overall_records)
-    finances = logic.extract_all_finances(records=overall_records)
+    finances = extract_all_finances(records=overall_records)
     finances = [finance.to_dict() for finance in finances]
     return {"items": finances, "records": overall_records}
 
 
-@api.get("/finances/{company_title}")
-def get_all_finances(company_title: str, overall_records: int = 100):
-    finances = logic.extract_company_finances(
-        company=company_title, records=overall_records
-    )
+@api.get("/finances/{company}")
+def get_all_finances(company: str, records: int = 100):
+    finances = extract_company_finances(company=company, records=records)
     finances = [finance.to_dict() for finance in finances]
-    return {"items": finances, "records": overall_records}
+    return {"items": finances, "records": records}
