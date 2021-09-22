48aa75aec59c623f42632bdf403595c601adc269
Ilkin Ramazanov <81832084+IlkinR@users.noreply.github.com>
2021-09-22 19:33:56 +0400

    Delete settings.py

--- settings.py
+++
@@ -1,3 +0,0 @@
-DATA_DOWNLOAD_URL_TEMPLATE = "https://query1.finance.yahoo.com/v7/finance/download/{title}?period1={start}&period2={end}&interval={interval}&events=history&includeAdjustedClose=true"
-
-DATABASE_NAME = "comp_finances.db"
