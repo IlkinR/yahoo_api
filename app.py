67754675527b4cceb9ca8c940925682c2dacc9d0
Ilkin Ramazanov <babalke44@gmail.com>
2021-09-22 19:13:09 +0400

    make some cleaning

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
 import logic
-from playhouse.shortcuts import model_to_dict, dict_to_model
-
 
 api = FastAPI()
 
@@ -16,7 +14,7 @@ def get_all_finances(overall_records: int = 100):
 @api.get("/finances/{company_title}")
 def get_all_finances(company_title: str, overall_records: int = 100):
     finances = logic.extract_company_finances(
-        company_name=company_title, records=overall_records
+        company=company_title, records=overall_records
     )
     finances = [finance.to_dict() for finance in finances]
     return {"items": finances, "records": overall_records}
--- logic.py
+++ logic.py
@@ -6,17 +6,17 @@ import settings
 
 
 def save_company_finances(
-    company_title: str,
+    company: str,
     start: int = 0,
     end: int = int(time.time()),
     interval: str = "1d",
 ) -> None:
     download_url = settings.DATA_DOWNLOAD_URL_TEMPLATE.format(
-        title=company_title, start=start, end=end, interval=interval
+        title=company, start=start, end=end, interval=interval
     )
     company_dataframe = pd.read_csv(download_url)
 
-    company_dataframe.insert(loc=0, column="company", value=company_title)
+    company_dataframe.insert(loc=0, column="company", value=company)
     inserted_cols = company_dataframe.columns.str.lower().str.split().str.join("_")
     renaming_data = dict(zip(company_dataframe.columns, inserted_cols))
     company_dataframe = company_dataframe.rename(renaming_data, axis=1)
@@ -26,18 +26,15 @@ def save_company_finances(
         CompanyFinance.insert_many(company_data).execute()
 
 
-def extract_company_finances(
-    company_name: str, records: int = 50
-) -> List[CompanyFinance]:
+def extract_company_finances(company: str, records: int = 50) -> List[CompanyFinance]:
     with database.atomic():
-        company_mask = CompanyFinance.company == company_name
+        company_mask = CompanyFinance.company == company
         by_company = CompanyFinance.select().where(company_mask)
         if by_company.count() == 0:
-            # there is no records for company, so we'll load them
-            save_company_finances(database, company_name)
+            save_company_finances(database, company)
 
     with database.atomic():
-        company_mask = CompanyFinance.company == company_name
+        company_mask = CompanyFinance.company == company
         by_company = CompanyFinance.select().where(company_mask)
         return by_company[:records]
 
